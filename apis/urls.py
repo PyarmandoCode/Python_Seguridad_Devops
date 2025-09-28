9a038281e9f1f3046c2dc1c3d813ab74435016e5
hola.pyarmando.academy@gmail.com <hola.pyarmando.academy@gmail.com>
2025-09-28 18:51:50 -0500

    Sesion3

--- apis/urls.py
+++ apis/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path,include
 from rest_framework.routers import DefaultRouter
-from .views import ArticuloViewSet
+from .views import ArticuloViewSet,ArticulosListCreateAPIView
 
-router=DefaultRouter()
-router.register('articulos',ArticuloViewSet)
+# router=DefaultRouter()
+# router.register('articulos',ArticuloViewSet)
 
 urlpatterns = [
-    path('',include(router.urls))
+    #path('',include(router.urls)),
+    path('articuloslist/',ArticulosListCreateAPIView.as_view())
 ]
